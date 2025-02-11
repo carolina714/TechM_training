12ffd1825e0bb4ab606707a4a8b8254d0623cccd
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-11 16:20:19 +0530

    Create Testm.java

---
+++ java/week2/Abstraction/Testm.java
@@ -0,0 +1,11 @@
+package org.example.assignment.abstracteg;
+
+public class Testm {
+    public static void main(String[] args){
+        Dog d=new Dog();
+        Owl o=new Owl();
+
+        d.makeSound();
+        o.makeSound();
+    }
+}
