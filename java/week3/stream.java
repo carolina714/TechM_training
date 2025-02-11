813a50257fa34d1adfd597fcf63fc95c3a203862
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-11 16:32:28 +0530

    Create stream.java

---
+++ java/week3/stream.java
@@ -0,0 +1,17 @@
+package org.example.assignment;
+import java.io.*;
+public class stream {
+    public static void main(String [] args)
+    {
+        System.out.print("System.print.out!\n");
+        String name="Game Of Thrones";
+        System.out.print("Enter Movie index: "+ name);
+ 
+        System.out.println("Team 9 !");
+        System.out.println("TvD...TO....L");
+        System.out.println("Watch the "+name+" now on Prime video!");
+
+        System.err.print("Error: NoCodeFound");
+        System.err.println("Error: NoGo");
+    }
+}
