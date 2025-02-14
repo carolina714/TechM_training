037966d0e57c957c8aca14399e9e1ad9ebcf0d3b
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-14 11:41:03 +0530

    Update Task_3.js

--- java/Javascript | 14.02.2025/Task_3.js
+++ java/Javascript | 14.02.2025/Task_3.js
@@ -1,8 +1,15 @@
-var number = -7; // You can change this value to any number
-if (number > 0) {
-  console.log("The number", number, "is positive.");
-} else if (number < 0) {
-  console.log("The number", number, "is negative.");
+const number = 29; // You can change this value to any number
+
+function isPrime(num) {
+  if (num <= 1) return false; // Numbers less than or equal to 1 are not prime
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false; // If divisible by any number other than 1 and itself, it's not prime
+  }
+  return true;
+}
+
+if (isPrime(number)) {
+  console.log("The number", number, "is a prime number.");
 } else {
-  console.log("The number is zero.");
+  console.log("The number", number, "is not a prime number.");
 }
