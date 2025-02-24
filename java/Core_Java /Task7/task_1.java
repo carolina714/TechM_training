2f035146fb20b8dfb645c79ac42da78c492efe1b
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-24 18:56:58 +0530

    Create task_1.java

---
+++ java/Core_Java /Task7/task_1.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Task1 {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+        System.out.println("Color List: " + colors);
+    }
+}
