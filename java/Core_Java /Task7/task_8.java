52a040944bbc156d936415fe12c515f7121e8cd5
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-24 19:01:25 +0530

    Create task_8.java

---
+++ java/Core_Java /Task7/task_8.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Task8 {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>(java.util.List.of("Yellow", "Blue", "Green", "Red"));
+        Collections.sort(colors);
+        System.out.println(colors);
+    }
+}
