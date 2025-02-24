8a87f1c7bed99daf4da9e4cbfe49f2647d50d44e
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-24 19:25:40 +0530

    Create task_18.java

---
+++ java/Core_Java /Task7/task_18.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class Task18 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.addLast("End");
+        System.out.println(list);
+    }
+}
