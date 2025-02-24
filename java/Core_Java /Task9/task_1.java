dee338109053d80406d210187ab913de6fc3cc02
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-24 19:35:53 +0530

    Create task_1.java

---
+++ java/Core_Java /Task9/task_1.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class Task1 {
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(10, 20, 30, 40, 50);
+
+        OptionalDouble average = numbers.stream()
+                .mapToInt(Integer::intValue)
+                .average();
+
+        System.out.println("Average: " + (average.isPresent() ? average.getAsDouble() : "No elements"));
+    }
+}
