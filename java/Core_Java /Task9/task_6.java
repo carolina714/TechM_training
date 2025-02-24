8233efc873040f3e1848596b7f74fdd7c38094b4
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-24 19:38:43 +0530

    Create task_6.java

---
+++ java/Core_Java /Task9/task_6.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Task6 {
+    public static void main(String[] args) {
+        List<String> words = List.of("banana", "apple", "cherry", "date", "elderberry");
+
+        List<String> ascendingOrder = words.stream()
+                .sorted()
+                .collect(Collectors.toList());
+
+        List<String> descendingOrder = words.stream()
+                .sorted((a, b) -> b.compareTo(a))
+                .collect(Collectors.toList());
+
+        System.out.println("Sorted in Ascending Order: " + ascendingOrder);
+        System.out.println("Sorted in Descending Order: " + descendingOrder);
+    }
+}
