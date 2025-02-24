f4e7d45b23ce6931872754400b4590ef0abf3900
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-24 19:22:05 +0530

    Create task_14.java

---
+++ java/Core_Java /Task7/task_14.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.Collections;
+
+public class Task14 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>(java.util.List.of("Apple", "Banana", "Cherry"));
+        Collections.reverse(list);
+        for (String item : list) {
+            System.out.println(item);
+        }
+    }
+}
