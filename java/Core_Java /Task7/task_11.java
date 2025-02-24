053cfae8f03d0ef4b228b8aab3f32d3e8a36ead4
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-24 19:20:24 +0530

    Create task_11.java

---
+++ java/Core_Java /Task7/task_11.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+
+public class Task11 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Apple");
+        list.addLast("Banana");
+        System.out.println(list);
+    }
+}
