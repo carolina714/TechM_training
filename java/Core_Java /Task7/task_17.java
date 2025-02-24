808235621838ded982ae850cd2e53a4c4276ad88
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-24 19:25:09 +0530

    Create task_17.java

---
+++ java/Core_Java /Task7/task_17.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+
+public class Task17 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.addFirst("Front");
+        System.out.println(list);
+    }
+}
