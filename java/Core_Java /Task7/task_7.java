7772eb2e014d709097c00d69d0d6fd4f03bf497d
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-24 19:00:54 +0530

    Create task_7.java

---
+++ java/Core_Java /Task7/task_7.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Task7 {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>(java.util.List.of("Red", "Blue", "Green", "Yellow"));
+        System.out.println("Contains Blue? " + colors.contains("Blue"));
+    }
+}
