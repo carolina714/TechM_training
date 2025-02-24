458f4da9be32959d62d3aa1016e83f5a03ea8d31
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-24 18:59:43 +0530

    Create task_5.java

---
+++ java/Core_Java /Task7/task_5.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Task5 {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>(java.util.List.of("Red", "Blue", "Green"));
+        colors.set(1, "Purple");
+        System.out.println(colors);
+    }
+}
