153318a8161bb5fdca289fcbcce191480aa63294
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-24 18:59:05 +0530

    Create task_4.java

---
+++ java/Core_Java /Task7/task_4.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class Task4 {
+    public static void main(String[] args) {
+        ArrayList<String> colors = new ArrayList<>(java.util.List.of("Red", "Blue", "Green", "Yellow"));
+        System.out.println("Element at index 2: " + colors.get(2));
+    }
+}
