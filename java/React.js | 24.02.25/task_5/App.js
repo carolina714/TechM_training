4f4f1707c2ed6cabec926e16d4ae8c5af845f750
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-25 08:13:59 +0530

    Create App.js

---
+++ java/React.js | 24.02.25/task_5/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Game from './Game'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Game name="Soccer" players={22} /> 
+    </div>
+  );
+}
+
+export default App;
