2533ca7a06b27edf02721995db5406189c697281
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-25 08:22:40 +0530

    Create app.js

---
+++ java/React.js | 24.02.25/task_6/app.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Training from './Training'; 
+
+function App() {
+  return (
+    <div className="App">
+      <Training />
+    </div>
+  );
+}
+
+export default App;
