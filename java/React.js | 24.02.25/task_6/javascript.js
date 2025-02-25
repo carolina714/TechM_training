db550b60f6de97cf53899bfbbcb0fe9179e3bb2b
carolina714 <116965874+carolina714@users.noreply.github.com>
2025-02-25 08:24:17 +0530

    Create javascript.js

---
+++ java/React.js | 24.02.25/task_6/javascript.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function Javascript() {
+  return (
+    <div className="topic">
+      <h2>JavaScript</h2>
+      <p>JavaScript is a dynamic programming language used for web development.</p>
+    </div>
+  );
+}
+
+export default Javascript;
